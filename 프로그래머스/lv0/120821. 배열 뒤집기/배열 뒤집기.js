221f1cde0b1dcc70fca876c096faf90bad0edbd2
wookki <103362820+wookki@users.noreply.github.com>
2023-03-10 04:25:24 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,3 +1,8 @@
 function solution(num_list) {
-    return num_list.reverse()
+    var answer = [];
+    var j = num_list.length
+    for(var i = 1; i <= j; i++){
+        answer.push(num_list[j-i])
+    }
+    return answer;
 }
