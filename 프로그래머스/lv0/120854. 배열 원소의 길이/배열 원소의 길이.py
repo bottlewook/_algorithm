147e4f6d70b2ae30286631f49fb06e634b75506c
wookki <103362820+wookki@users.noreply.github.com>
2023-03-13 21:37:54 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,8 +1,5 @@
 def solution(strlist):
     arr = []
     for str in strlist:
-        sum = 0
-        for num in str:
-            sum += 1
-        arr.append(sum)
+        arr.append(len(str))
     return arr
