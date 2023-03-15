7770bed9d389eb8ef7ea93724f830256fd2cc221
wookki <103362820+wookki@users.noreply.github.com>
2023-03-15 12:58:33 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,17 +1,2 @@
 def solution(array):
-    answer = []
-    max = array[0]
-    idx = 0
-    temp = 0
-    
-    for num in array:
-        temp += 1
-        print(temp)
-        if num > max:
-            max = num
-            idx = temp - 1
-            print(idx)
-        
-    answer.append(max)
-    answer.append(idx)
-    return answer
+    return [max(array), array.index(max(array))]
