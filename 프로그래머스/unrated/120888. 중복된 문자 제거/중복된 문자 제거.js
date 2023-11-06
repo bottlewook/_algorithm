9a673b40af8ed9e5dbd628daf2d9b8806bb858e3
wookki <103362820+wookki@users.noreply.github.com>
2023-11-06 22:04:22 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++) {
+        if(i === my_string.indexOf(my_string[i])) answer += my_string[i]
+    }
+    return answer;
+}
