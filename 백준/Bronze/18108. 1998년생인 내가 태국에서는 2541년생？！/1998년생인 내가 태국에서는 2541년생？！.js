426018c1559dbd79b5a7863079c9f21a45837f8f
wookki <103362820+wookki@users.noreply.github.com>
2023-11-15 20:41:48 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 144 ms, Memory: 9708 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,38 @@
+/* readline Module */
+
+// 문제 풀이
+function solution(input) {
+
+	// 답변 출력, list
+	console.log(input - 543);
+   
+}
+
+
+
+/* readline Module */
+const readline = require("readline");
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout,
+    
+});
+
+
+let input;
+let list = [];
+
+rl.on("line", function (line) {
+
+    input = line; // 입력받은 문자열, line
+    rl.close();   // 입력 종료
+    
+}).on("close", function () {
+
+    // 형변환, 구분자(띄어쓰기)기준으로 배열에 삽입
+    list = input.split(' ')
+
+    solution(list); // 문제 풀이 함수 호출
+    process.exit(); // 프로세스 종료
+});
