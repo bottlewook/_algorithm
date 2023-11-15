7258d5db0bbf672547ede576f505d2d85c347f66
wookki <103362820+wookki@users.noreply.github.com>
2023-11-15 21:31:48 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 148 ms, Memory: 9444 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,37 @@
+// 문제 풀이
+function solution(input) {
+    const [A, B] = input
+	// 답변 출력, list
+	if (A > B) console.log('>')
+	else if (A < B) console.log('<')
+    else console.log('==')
+}
+
+
+
+/* readline Module */
+const readline = require("readline");
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout,
+    
+});
+
+
+let input;
+let list = [];
+
+rl.on("line", function (line) {
+
+    input = line; // 입력받은 문자열, line
+    rl.close();   // 입력 종료
+    
+}).on("close", function () {
+
+    // 형변환, 구분자(띄어쓰기)기준으로 배열에 삽입
+    list = input.split(' ').map((el) => Number(el));
+
+    solution(list); // 문제 풀이 함수 호출
+    process.exit(); // 프로세스 종료
+});
