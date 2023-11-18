575a7f5b1ee60bb666da8973c501456cf7f7c0d4
wookki <103362820+wookki@users.noreply.github.com>
2023-11-18 14:20:56 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,37 +1,21 @@
 function solution(N, stages) {
     var answer = [];
-    const stageMap = new Map();
+    
+    let map = new Map();
+    
     for (let stage of stages) {
-        if (stage > N) continue;
-        if (stageMap.has(stage)) stageMap.set(stage, stageMap.get(stage) + 1)
-        else stageMap.set(stage, 1);
-    };
-    
-    for (let i = 1; i <= N; i++) {
-        if (!stageMap.has(i)) stageMap.set(i, 0)
+        if (map.has(stage)) map.set(stage, map.get(stage) + 1);
+        else map.set(stage, 1);
     }
     
-    const stageMapArr = [...stageMap];
+    let arr = [];
     
-    stageMapArr.sort((a, b) => a[0] - b[0])
-    
-    const sortedStageMap = new Map(stageMapArr)
-    
-    let people = stages.length
-    const scoreMap = new Map();
-    
-    for (let [key, value] of sortedStageMap) {
-        scoreMap.set(key, value / people);
-        people -= value;
+    for (let [key, value] of map) {
+        arr.push([key, value])
     }
     
-    const scoreMapArr = [...scoreMap];
-    scoreMapArr.sort((a, b) => {
-        if (a[1] === b[1]) return a[0] - b[0];
-        else return b[1] - a[1]
-    })
+    arr.sort((a, b) => b[0] - a[0]);
     
-    answer = scoreMapArr.map(score => score[0])
-    
-    return answer
+    console.log(arr)
+    return answer;
 }
