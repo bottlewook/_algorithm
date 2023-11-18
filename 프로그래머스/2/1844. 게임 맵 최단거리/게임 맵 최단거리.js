470dc9b6e03ae2cfe4a99b6648a60426b5a76e6f
wookki <103362820+wookki@users.noreply.github.com>
2023-11-19 02:06:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.81 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,4 +1,28 @@
 function solution(maps) {
-    var answer = 0;
-    return answer;
+    let answer = 1000
+
+    let n = maps.length;
+    let m = maps[0].length
+    let dx = [-1, 0, 1, 0];
+    let dy = [0, 1, 0, -1];
+    
+    maps[0][0] = 0;
+    let queue = [];
+    
+    queue.push([0, 0, 1]);
+    
+    while (queue.length) {
+        let [x, y, count] = queue.shift();
+        if (x === n - 1 && y === m - 1) answer = Math.min(answer, count)
+        for (let k = 0; k < 4; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] === 1) {
+                maps[nx][ny] = 0;
+                queue.push([nx, ny, count + 1])
+            }
+        }
+    }
+    
+    return answer === 1000 ? -1 : answer;
 }
