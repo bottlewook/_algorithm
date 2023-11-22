345fc83a09b62fc808741ea9aa9df2be66a5b50e
wookki <103362820+wookki@users.noreply.github.com>
2023-11-22 11:21:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,16 @@
 function solution(n, words) {
-  let store = [words[0]];
-  let temp = words[0][words[0].length - 1];
+    const answer = [0, 0];
+    const duplication = [];
+    const length = words.length;
 
-  for (let i = 1; i < words.length; i++) {
-    // 중복된 단어가 있을 경우
-    if (store.includes(words[i]))
-      return [(i + 1) % n === 0 ? n : (i + 1) % n, Math.floor(i / n) + 1];
-    // 앞뒤 단어가 다른 경우
-    if (words[i][0] !== temp)
-      return [(i + 1) % n === 0 ? n : (i + 1) % n, Math.floor(i / n) + 1];
+    let turn = 1;
+    duplication.push(words[0])
+    for (let i = 1; i < words.length; i++) {
+        if (i % n === 0) turn++
+        if (duplication.includes(words[i])) return [(i % n) + 1, turn]
+        duplication.push(words[i]);
+        if (words[i - 1].charAt(words[i - 1].length - 1) !== words[i].charAt(0)) return [(i % n) + 1, turn]
+    }
 
-    temp = words[i][words[i].length - 1];
-
-    store.push(words[i]);
-  }
-
-  return [0, 0];
+    return answer;
 }
