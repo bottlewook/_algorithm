5a7a8711e4b77edd6550dd6e5b0e4d8074635e86
wookki <103362820+wookki@users.noreply.github.com>
2023-11-23 19:57:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 64.59 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -4,27 +4,28 @@ function shiftArray(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         arr[i] = arr[i + 1];
     }
+    
     arr[arr.length - 1] = temp;
     
     return arr
 }
 
 function solution(elements) {
-    var answer = new Set(elements);
-    let arr = [...elements]
+    const answer = new Set(elements);
+    const array = [...elements];
     
-    let count = 1;
+    let count = 1
     
     while (count < elements.length) {
+        shiftArray(array);
         
-        shiftArray(arr)
-        
-        for (let k = 0; k < elements.length; k++) {
-            elements[k] += arr[k]
-            answer.add(elements[k])
+        for (let i = 0; i < elements.length; i++) {
+            elements[i] += array[i]
+            answer.add(elements[i])
         }
         
         count++
     }
+    
     return answer.size
 }
