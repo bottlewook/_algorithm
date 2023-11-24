faffb1a9499ca5315ce37494487ea0865cc26a06
wookki <103362820+wookki@users.noreply.github.com>
2023-11-24 17:13:46 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,30 +1,28 @@
 function solution(want, number, discount) {
     var answer = 0;
     let obj = {};
-    
+    let sum = number.reduce((a, b) => a + b, 0)
     
     for (let i = 0; i < want.length; i++) {
         obj[want[i]] = number[i]
     }
     
-    let lt = 0;
-    let rt = 10;
-    
-    while (rt <= discount.length) {
-       let temp = { ...obj } 
-       let flag = 1;
-       for (let i = lt; i < rt; i++) {
-         if (temp[discount[i]]) temp[discount[i]]--
-         
-       }
-        for(let value of Object.values(temp)) {
-            if(value > 0) {
+    for (let i = 0; i <= discount.length - sum; i++) {
+        const temp = { ...obj };
+        for (let j = i; j < i + sum; j++) {
+            if ((temp[discount[j]])) temp[discount[j]]--
+        }
+        
+        let flag = 1;
+        
+        for (let value of Object.values(temp)) {
+            if (value > 0) {
                 flag = 0;
+                break
             }
         }
-        if(flag) answer++
-        rt++
-        lt++
+        if (flag) answer++
     }
+    
     return answer;
 }
