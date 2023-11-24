cdc00989efaf193c745128370b5b29082c1dd4a4
wookki <103362820+wookki@users.noreply.github.com>
2023-11-24 16:06:54 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,30 +1,37 @@
-function check(arr) {
-  let stack = [];
-
-  for (let x of arr) {
-    if (x === "[" || x === "{" || x === "(") {
-      stack.push(x);
-    } else {
-        if (x === ']' && stack[stack.length - 1] === '[') stack.pop();
-        else if (x === ')' && stack[stack.length - 1] === '(') stack.pop();
-        else if (x === '}' && stack[stack.length - 1] === '{') stack.pop();
-        else return false
+function shiftArr(arr) {
+    let temp = arr.at(0);
+    
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1]
     }
-  }
-
-  return !stack.length;
+    
+    arr[arr.length - 1] = temp;
+    
+    return arr
 }
 
 function solution(s) {
-  let answer = 0;
-
-  for (let i = 1; i <= s.length; i++) {
-    if (check(s)) answer++;
-    let temp = s;
-    let val = temp[0];
-    let str = temp.substring(1);
-    s = str + val;
-  }
-
-  return answer;
+    let answer = 0;
+    let stack = [];
+    const length = s.length;
+    const arr = s.split('');
+    
+    let count = 0;
+    
+    while (count < length) {
+        
+        for (let i = 0; i < length; i++) {
+            if (stack.at(-1) === '[' && arr[i] === ']') stack.pop();
+            else if (stack.at(-1) === '(' && arr[i] === ')') stack.pop();
+            else if (stack.at(-1) === '{' && arr[i] === '}') stack.pop();
+            else stack.push(arr[i])
+        }
+        if (stack.length === 0) answer++ 
+        
+        stack = [];
+        shiftArr(arr)
+        count++
+    }
+    
+    return answer
 }
