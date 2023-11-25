9d73fa4627362b63ed5ead0fb7a6ee655d704082
wookki <103362820+wookki@users.noreply.github.com>
2023-11-25 17:23:30 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,15 @@
 function solution(clothes) {
     let answer = 1;
+    const map = new Map();
     
-    let map = new Map();
-    
-    for (let [key, value] of clothes) {
-        if (map.has(value)) {
-            map.set(value, map.get(value) + 1)
-        } else {
-            map.set(value, 1)
-        }
-    }
+    for (let cloth of clothes) {
+        if (map.has(cloth[1])) map.set(cloth[1], map.get(cloth[1]) + 1);
+        else map.set(cloth[1], 1)
+    };
     
     for (let [key, value] of map) {
         answer *= (value + 1)
-    } 
-    
+    }
     
     return answer - 1
 }
