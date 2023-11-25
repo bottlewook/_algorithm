6b5443704c472524411eb872e5df736ba667c47a
wookki <103362820+wookki@users.noreply.github.com>
2023-11-26 01:17:40 +0900

    [level 2] Title: [1차] 캐시, Time: 2.40 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,30 +1,32 @@
 function solution(cacheSize, cities) {
-    var answer = 0;
-    const cache = Array.from({length: cacheSize}, () => 0);
-    
-    const lowerCities = cities.map(city => city.toLowerCase())
+    if (cacheSize === 0) return cities.length * 5
+    let answer = 0;
+    const lowerCities = cities.map(city => city.toLowerCase());
+    const cache = Array.from({ length: cacheSize }, () => 0);
     
     lowerCities.forEach(city => {
-       let position = -1;
+        let index = -1;
         
-       for (let i = 0; i < cacheSize; i++) if (cache[i] === city) position = i;
+        for (let i = 0; i < cache.length; i++) {
+            if (cache[i] === city) index = i    
+        };
         
-        if (position == -1) {
-            for (let i = cacheSize - 1; i > 0; i--) {
-                cache[i] = cache[i - 1]
+        if (index !== -1) {
+            for (let i = index; i > 0; i--) {
+                cache[i] = cache[i - 1];
+            };
+            
+            answer++
+        } else {
+            for (let i = cache.length - 1; i > 0; i--) {
+                cache[i] = cache[i - 1];
             }
             
             answer += 5
-        } else {
-            for (let i = position; i > 0; i--) {
-                cache[i] = cache[i - 1]
-            }
-            
-            answer += 1
         }
         
         cache[0] = city
     })
-        
+    
     return answer;
 }
