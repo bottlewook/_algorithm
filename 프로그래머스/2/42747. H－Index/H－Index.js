bbfca8795ff9ce28ecab97f8fff2457bc03fe539
wookki <103362820+wookki@users.noreply.github.com>
2023-11-25 11:07:52 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,23 +1,24 @@
-function solution(citations) {
-    var answer = 0;
-    citations.sort((a, b) => a - b);
+function hIndex(arr, number) {
+    let check = 0;
     
+    for (i = 0; i < arr.length; i++) {
+        if (number <= arr[i]) check++
+    }
+    
+    return check >= number
+}
+
+function solution(citations) {
+    let answer = 0;
     let lt = 0;
-    let rt = Math.max(...citations)
+    let rt = Math.max(...citations);
     
     while (lt <= rt) {
-        // 인용 횟수의 최대값
         let mid = Math.floor((lt + rt) / 2);
-        let count = 0;
-        for (let i = 0; i < citations.length; i++) {
-            if (mid < citations[i]) count++
-        }
-        if (count === mid) return mid
-        if (count < mid) {
-            rt = mid - 1
-        } else {
+        if (hIndex(citations, mid)) {
+            answer = Math.max(mid, answer);
             lt = mid + 1
-        }
+        } else rt = mid - 1
     }
     
     return answer;
