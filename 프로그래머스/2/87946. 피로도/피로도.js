1ffa5a55b5b19073ff5dcffccc928ff0841c7ed7
wookki <103362820+wookki@users.noreply.github.com>
2023-11-28 01:23:21 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,20 +1,19 @@
 function solution(k, dungeons) {
-    let answer = -1;
+    let answer = 0;
     const checkList = Array.from({ length: dungeons.length }, () => 0);
     
     function DFS(level, hp) {
-        for (let i = 0; i < dungeons.length; i++) {
-            if (checkList[i] === 0 && hp >= dungeons[i][0]) {
+        answer = Math.max(answer, level);
+        for (let i = 0; i < checkList.length; i++) {
+            if (checkList[i] === 0 && dungeons[i][0] <= hp) {
                 checkList[i] = 1;
-                DFS(level + 1, hp - dungeons[i][1]);
+                DFS(level + 1, hp - dungeons[i][1])
                 checkList[i] = 0;
             }
         }
-        
-        answer = Math.max(answer, level)
-    }
+    };
     
     DFS(0, k);
     
-    return answer;
+    return answer
 }
