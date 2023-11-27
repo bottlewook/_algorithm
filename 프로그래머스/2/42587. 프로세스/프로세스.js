57b86b6c272aa77a6d98eb8e67fe77ec7dd55d7b
wookki <103362820+wookki@users.noreply.github.com>
2023-11-27 23:48:11 +0900

    [level 2] Title: 프로세스, Time: 0.65 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,23 +1,4 @@
 function solution(priorities, location) {
-    const answer = [];
-    const queue = Array.from({ length: priorities.length }, () => 0);
-    
-    for (let i = 0; i < queue.length; i++) {
-        queue[i] = [String.fromCharCode(i + 65) ,priorities[i]]
-    }; 
-    
-    while (queue.length) {
-        const value = queue.shift();
-        
-        let flag = 1;
-        for (let i = 0; i < queue.length; i++) {
-            if (value[1] < queue[i][1]) flag = 0;
-        }
-        if (flag) answer.push(value)
-        else queue.push(value)
-    }
-    
-    for (let i = 0; i < answer.length; i++) {
-        if (location === answer[i][0].charCodeAt() - 65) return i + 1
-    }
+    var answer = 0;
+    return answer;
 }
