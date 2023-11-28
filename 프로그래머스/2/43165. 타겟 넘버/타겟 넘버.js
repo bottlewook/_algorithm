0c506280deb1b97c5365d7ae2e583ae479b04f0c
wookki <103362820+wookki@users.noreply.github.com>
2023-11-28 21:41:13 +0900

    [level 2] Title: 타겟 넘버, Time: 1.13 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,4 @@
 function solution(numbers, target) {
-    let answer = 0;
-    
-    function DFS(level, sum) {
-        if (level === numbers.length) {
-            if (sum === target) answer++;
-        }
-        else {
-            DFS(level + 1, sum + numbers[level]) 
-            DFS(level + 1, sum - numbers[level])    
-        }
-    };
-    
-    DFS(0, 0)
-    return answer
+    var answer = 0;
+    return answer;
 }
