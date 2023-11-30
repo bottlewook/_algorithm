da5f98474baa57f64fd5893397322c4806c107d2
wookki <103362820+wookki@users.noreply.github.com>
2023-11-30 10:22:51 +0900

    [level 2] Title: 스킬트리, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,22 +1,16 @@
 function solution(skill, skill_trees) {
     let answer = 0;
-    let skillArr = Array.from(skill);
+    const queue = skill.split('');
     
-    for (let skill_tree of skill_trees) {
-        let flag = 0;
-        let temp = [...skillArr]
-        for (let word of skill_tree) {
-            if (flag) break
-            if (temp.includes(word)) {
-                if (temp[0] !== word) {
-                    flag = 1;
-                    break;
-                }
-                else temp.shift()
+    for (let skill of skill_trees) {
+        const temp = [...queue];
+        let flag = 1;
+        for (let word of skill) {
+            if (temp.includes(word) && flag) {
+                if (temp.shift() !== word) flag = 0;
             }
         }
-        
-        if (!flag) answer++
+        if (flag) answer++
     }
     
     return answer;
