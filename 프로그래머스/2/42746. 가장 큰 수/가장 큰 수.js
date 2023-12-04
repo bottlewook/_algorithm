b6400fc437740b891ea0f1fffc4f09ee896e2c89
wookki <103362820+wookki@users.noreply.github.com>
2023-12-04 10:30:40 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,4 @@
 function solution(numbers) {
     var answer = '';
-    answer = numbers.map(number => String(number)).sort((a, b) => (b + a) - (a + b)).join('')
-    return answer[0] === '0' ? '0' : answer
+    return answer;
 }
