9569d28ddff058f20184208cac1f11e6f8218463
임병욱 <103362820+wookki@users.noreply.github.com>
2024-01-14 20:04:42 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n').map(item => +item);
+
+let max = 0;
+let index = 0;
+for (let i = 0; i < input.length; i++) {
+    if (max < input[i]) {
+        max = input[i]
+        index = i + 1
+    }
+}
+
+console.log(max)
+console.log(index)
