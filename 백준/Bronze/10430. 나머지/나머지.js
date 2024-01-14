5cab1302345b0151fa2903eff934f4a3baa7758c
wookki <103362820+wookki@users.noreply.github.com>
2024-01-14 15:21:46 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.js
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -1,44 +1,9 @@
-// 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ').map(item => +item);
 
-/* readline Module */
+const [A, B, C] = input
 
-// 문제 풀이
-function solution(input) {
-    const [A, B, C] = input
-	// 답변 출력, list
-	console.log((A + B) % C);
-	console.log(((A % C) + (B % C)) % C);
-	console.log((A * B) % C,);
-	console.log(((A % C) * (B % C)) % C);
-    
-   
-}
-
-
-
-/* readline Module */
-const readline = require("readline");
-const rl = readline.createInterface({
-
-    input: process.stdin,
-    output: process.stdout,
-    
-});
-
-
-let input;
-let list = [];
-
-rl.on("line", function (line) {
-
-    input = line; // 입력받은 문자열, line
-    rl.close();   // 입력 종료
-    
-}).on("close", function () {
-
-    // 형변환, 구분자(띄어쓰기)기준으로 배열에 삽입
-    list = input.split(' ').map((el) => Number(el));
-
-    solution(list); // 문제 풀이 함수 호출
-    process.exit(); // 프로세스 종료
-});
+console.log((A + B) % C)
+console.log(((A % C) + (B % C)) % C)
+console.log((A * B) % C)
+console.log(((A % C) * (B % C)) % C)
