476d9b49bc1e5382cd7626e5fd119b2a035d33c0
임병욱 <103362820+wookki@users.noreply.github.com>
2024-01-14 21:46:45 +0900

    [Bronze V] Title: A+B - 8, Time: 236 ms, Memory: 9544 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const count = Number(input[0]);
+
+for (let i = 1; i <= count; i++) {
+    const [a, b] = input[i].split(' ').map(item => Number(item));
+    console.log(`Case #${i}: ${a} + ${b} = ${a + b}`)
+}
