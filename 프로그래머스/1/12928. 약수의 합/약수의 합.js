a6123053da7d830ff862040695bd13037830f3f7
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-22 09:55:22 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = 0;
+    const arr = []
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) arr.push(i)
+    }
+    return arr.reduce((a, b) => a + b, 0)
+}
