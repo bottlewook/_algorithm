74e8ed21b9ab004530fbd71d3b2eef7c954743f2
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-23 09:37:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    numbers.sort((a, b) => a - b);
+    const answer = []
+    for (let i = 0; i <= 9; i++) {
+        if (!numbers.includes(i)) answer.push(i)
+    }
+    
+    return answer.reduce((a, b) => a + b, 0)
+}
