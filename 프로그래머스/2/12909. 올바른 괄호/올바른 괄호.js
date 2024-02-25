e427381d75cb7ef7dacd95b4d3e366a87f68be41
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-25 13:05:16 +0900

    [level 2] Title: 올바른 괄호, Time: 9.48 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,14 @@
-function solution(s) {
-    if (s[0] === ')') return false;
+function solution(s){
+    let answer = true;
     const stack = [];
-    
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') stack.push('(');
+
+    for (let x of s) {
+        if (x === '(') stack.push(x)
         else {
             if (stack.at(-1) === '(') stack.pop();
             else return false
         }
     }
 
-    return !stack.length;
+    return stack.length === 0;
 }
