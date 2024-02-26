8deb53242900c6630a2026093757705fbde1e2f5
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-26 10:33:32 +0900

    [level 2] Title: 피보나치 수, Time: 2.34 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,11 @@
+const memo = Array.from({ length: 999999}, () => 0)
+memo[0] = 0;
+memo[1] = 1;
+memo[2] = 1;
+
 function solution(n) {
-    const checkList = Array.from({ legnth: 100001 }, () => 0);
-    
-    checkList[0] = 0;
-    checkList[1] = 1;
-    
-    for (let i = 2; i <= n; i++) {
-        checkList[i] = (checkList[i - 1] + checkList[i - 2]) % 1234567
+    for (let i = 3; i <= n; i++) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567
     }
-    
-    return checkList[n]
+    return memo[n]
 }
