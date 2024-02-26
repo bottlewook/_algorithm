1ab5a787055ec4ffd0e48f5831cbe3d07b766a7f
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-26 10:01:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.20 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,22 +1,16 @@
-function solution(n) {
-    var answer = 0;
-    let binary = n.toString(2);
+function count(num) {
+    const temp = num.toString(2);
     let count = 0;
-    
-    for (let i = 0; i < binary.length; i++) {
-        if (binary[i] === '1') count++
+    for (let x of temp) {
+        if (x === '1') count++
     }
-    
+    return count
+}
+
+function solution(n) {
+    let temp = count(n)
     while (true) {
-        let value = n + 1
-        let str = value.toString(2)
-        let count1 = 0;
-        for (let i = 0; i < str.length; i++) {
-            if (str[i] === '1') count1++
-        }
-        if(count === count1) return n + 1
         n++
-    };
-    
-    return answer;
+        if (temp === count(n)) return n
+    }
 }
