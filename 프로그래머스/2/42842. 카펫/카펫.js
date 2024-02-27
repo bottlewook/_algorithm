5a961d12d0540d39d87b19ae2c6c16128e888f1d
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-02-27 10:16:49 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,14 @@
 function solution(brown, yellow) {
-    const rows = [];
-    const sum = brown + yellow;
-    
-    for (let row = sum - 1; row >= 2 && row ** 2 >= sum; row--) {
-        if (sum % row === 0) rows.push(row)
+    const answer = [];
+    const total = brown + yellow
+    for (let col = 1; col <= total; col++) {
+        const row = total / col
+        const edge = col * 2 + (row - 2) * 2 
+        if (brown === edge) {
+            answer.push(row)
+            answer.push(col)
+            break
+        }
     }
-    
-    for (let i = 0; i < rows.length; i++) {
-        let col = sum / rows[i];
-        if (col + rows[i] - 2 === brown / 2) return [rows[i], col]
-    }
+    return answer;
 }
