e7659d36ea0b6ab3ff0ccc86d1625883e7b428cd
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-05 09:11:39 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,11 @@
 function solution(n) {
     let answer = 0;
-
+    
     while (n !== 0) {
         if (n % 2 === 1) {
             answer++
-            n = (n - 1) / 2
-        } else n /= 2
+        }
+        n = Math.floor(n / 2)
     }
 
     return answer;
