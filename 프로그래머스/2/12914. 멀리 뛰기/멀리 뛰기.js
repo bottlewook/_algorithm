386d800c6a13b90b007f9558e0e0c22c622743f9
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-06 17:46:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,11 +1,10 @@
 function solution(n) {
-    const dp = Array.from({ length: 2001 }, () => 0);
-    dp[1] = 1;
-    dp[2] = 2;
-    
-    for (let i = 3; i <= n; i++) {
+    const dp = Array.from({ length: n }, () => 0);
+    dp[0] = 1;
+    dp[1] = 2;
+    for (let i = 2; i < n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
     }
     
-    return dp[n];
+    return dp[n - 1];
 }
