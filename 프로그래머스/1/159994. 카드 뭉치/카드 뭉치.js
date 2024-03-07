38465cb294077bc8112e29de21216dd3f2c2cdbf
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-07 10:22:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,39 +1,13 @@
 function solution(cards1, cards2, goal) {
-    let i = 0;
-    let j = 0;
-    let arr = [...cards1, ...cards2];
-    if(!arr.every(item => goal.includes(item))) return 'No'
-    
-    while (i < cards1.length && j < cards2.length) {
-        if (cards1[i] === goal[0]) {
-            i++;
-        } else if (cards2[j] === goal[0]) {
-            j++
-        } 
-        goal.shift();
-    }
-    
-    while (i < cards1.length) {
-        if (cards1[i] === goal[0]) {
-            i++;
-            goal.shift();
+    var answer = 'Yes';
+    for (let x of goal) {
+        if (cards1[0] === x) {
+            cards1.shift();
+        } else if (cards2[0] === x) {
+            cards2.shift();
         } else {
             return 'No'
         }
     }
-    
-    while (j < cards2.length) {
-        if (cards2[j] === goal[0]) {
-            j++;
-            goal.shift();
-        } else {
-            return 'No'
-        }
-    }
-    
-    if (i === cards1.length && j === cards2.length) {
-        return 'Yes'
-    }
-    
-    return 'No';
+    return answer;
 }
