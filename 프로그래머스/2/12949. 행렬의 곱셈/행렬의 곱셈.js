6d6a109b7a2b5342d216aa06c70bde41fd5b178a
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-13 13:44:06 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.19 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,14 +1,13 @@
 function solution(arr1, arr2) {
     const answer = [];
-    
     for (let i = 0; i < arr1.length; i++) {
         const arr = [];
         for (let j = 0; j < arr2[0].length; j++) {
-            let value = 0;
+            let temp = 0;
             for (let k = 0; k < arr1[0].length; k++) {
-                value += arr1[i][k] * arr2[k][j]
+                temp += arr1[i][k] * arr2[k][j]
             }
-            arr.push(value)
+            arr.push(temp);
         }
         answer.push(arr)
     }
