3c9905489ddb2ad1ca640c54cbd18116171402fc
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-13 13:55:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,16 +1,5 @@
 function solution(nums) {
-    let n = nums.length / 2;
-    let answer = 0;
-    let map = new Map();
-    
-    for (let x of nums) {
-        if (map.has(x)) map.set(x, map.get(x) + 1) 
-        else map.set(x, 1)
-    };
-    
-    for (let [key, value] of map) {
-        answer++
-    }
-    
-    return answer >= n ? n : answer
+    const count = nums.length / 2;
+    const set = new Set(nums)
+    return Math.min(count, set.size);
 }
