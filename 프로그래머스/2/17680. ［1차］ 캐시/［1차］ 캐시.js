3c63b60af914197776eb2922d5257c125d4c4247
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-18 11:55:13 +0900

    [level 2] Title: [1차] 캐시, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,31 +1,23 @@
 function solution(cacheSize, cities) {
+    if (cacheSize === 0) return cities.length * 5; // 캐시 크기가 0인 경우 처리
+
     let answer = 0;
-    const lowerCities = cities.map(city => city.toLowerCase());
-    const cache = Array.from({ length: cacheSize }, () => 0);
+    var cities = cities.map(item => item.toLowerCase());
+    const cache = [];
     
-    lowerCities.forEach(city => {
-        let index = -1;
-        
-        for (let i = 0; i < cacheSize; i++) {
-            if (cache[i] === city) index = i    
-        };
-        
-        if (index !== -1) {
-            for (let i = index; i > 0; i--) {
-                cache[i] = cache[i - 1];
-            };
-            
-            answer++
-        } else if (index === -1) {
-            for (let i = cache.length - 1; i > 0; i--) {
-                cache[i] = cache[i - 1];
-            }
-            
-            answer += 5
+    for (let city of cities) {
+        const index = cache.indexOf(city);
+        if (index > -1) {
+            // 캐시 히트: 해당 도시를 캐시에서 제거하고 가장 앞에 추가
+            cache.splice(index, 1);
+            answer += 1;
+        } else {
+            // 캐시 미스: 캐시가 꽉 찼다면 가장 오래된 항목 제거
+            if (cache.length >= cacheSize) cache.pop();
+            answer += 5;
         }
-        
-        cache[0] = city
-    })
+        cache.unshift(city); // 현재 도시를 캐시의 가장 앞에 추가
+    }
     
     return answer;
 }
