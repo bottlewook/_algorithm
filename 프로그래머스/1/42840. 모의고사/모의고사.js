d8308d64ec83f6e8dc34dae5f0094b9ebfa64377
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-18 16:46:04 +0900

    [level 1] Title: 모의고사, Time: 3.13 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,23 @@
 function solution(answers) {
-    var answer = [];
-    
-    let answer1 = 0;
-    let answer2 = 0;
-    let answer3 = 0;
-    
-    let person1 = [1, 2, 3, 4, 5];
-    let person2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    let person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
+    const answer = [];
+    let o = 0;
+    let t = 0;
+    let th = 0;
+    const arr = [0, 0, 0]
+    const one = [1, 2, 3, 4, 5];
+    const two = [2, 1, 2, 3, 2, 4, 2, 5];
+    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === person1[i % person1.length]) answer1++
-        if (answers[i] === person2[i % person2.length]) answer2++
-        if (answers[i] === person3[i % person3.length]) answer3++
-    };
-    
-    let newArr = [answer1, answer2, answer3];
-    let max = Math.max(...newArr)
-    
-    for (let i = 0; i < newArr.length; i++) {
-        if (newArr[i] === max) answer.push(i + 1)
+        if (one[i % one.length] === answers[i]) o++
+        if (two[i % two.length] === answers[i]) t++
+        if (three[i % three.length] === answers[i]) th++
     }
-    
+    const max = Math.max(o, t, th);
+    const temp = [o, t, th]
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === max) {
+            answer.push(i + 1)
+        }
+    }
     return answer;
 }
