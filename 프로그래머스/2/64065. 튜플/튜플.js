4c3a609ce0fc80e51e2fb3b89c245e5591aabaa9
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-18 15:30:34 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,25 +1,14 @@
 function solution(s) {
-    const answer = [];
-    const arr = s.replace(/[{}]/g, '').split(',').map(item => Number(item))
-
-    const map = new Map();
-    
+    var answer = [];
+    const set = new Set();
+    const temp = s.replaceAll('{', '[');
+    const string = temp.replaceAll('}', ']');
+    const arr = JSON.parse(string)
+    arr.sort((a, b) => a.length - b.length);
     for (let x of arr) {
-        if (map.has(x)) map.set(x, map.get(x) + 1);
-        else map.set(x, 1);
+        for (let y of x) {
+            set.add(y)
+        }
     }
-    
-    const temp = [];
-    
-    for (let [key, value] of map) {
-        temp.push([key, value])
-    }
-    
-    temp.sort((a, b) => b[1] - a[1]);
-    
-    for (let [a, b] of temp) {
-        answer.push(a)
-    }
-    
-    return answer
+    return [...set]
 }
