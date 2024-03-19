cfab0639d756eda9e0000b5c03124ccd8cd9c508
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-19 14:17:08 +0900

    [level 2] Title: 피로도, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,19 +1,18 @@
 function solution(k, dungeons) {
-    let answer = -1;
+    let answer = 0;
     const checkList = Array.from({ length: dungeons.length }, () => 0);
     
     function DFS(level, hp) {
-        for (let i = 0; i < checkList.length; i++) {
-            if (checkList[i] === 0 && hp >= dungeons[i][0]) {
-                checkList[i] = 1;
-                DFS(level + 1, hp - dungeons[i][1])
-                checkList[i] = 0;
+            for (let i = 0; i < dungeons.length; i++) {
+                if (checkList[i] === 0 && hp >= dungeons[i][0]) {
+                    checkList[i] = 1;
+                    answer = Math.max(answer, level)
+                    DFS(level + 1, hp - dungeons[i][1])
+                    checkList[i] = 0;
             }
         }
-        
-      answer = Math.max(answer, level)
     }
     
-    DFS(0, k)
+    DFS(1, k)
     return answer;
 }
