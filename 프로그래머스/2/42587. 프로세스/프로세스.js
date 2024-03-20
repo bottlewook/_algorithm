d0c35859bb0699032a40eb5bb1bf294dbff8d4e4
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-21 00:45:44 +0900

    [level 2] Title: 프로세스, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,4 +1,22 @@
 function solution(priorities, location) {
-    var answer = 0;
-    return answer;
+    let answer = 0;
+    const queue = priorities.map((priority, index) => {
+        return {
+            priority, index
+        }
+    })
+    
+    while (queue.length > 0) {
+        const current = queue.shift();
+        const max = queue.some(item => item.priority > current.priority);
+        
+        if (max) {
+            queue.push(current)
+        } else {
+            answer++
+            if (current.index === location) break
+        }
+    }
+        
+    return answer
 }
