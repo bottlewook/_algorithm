64ba1ebe90354d076ff5f21efec0420fd26a86d3
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-21 22:06:06 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,17 @@
 function solution(numbers, target) {
-    var answer = 0;
+    let answer = 0;
+    
+    function DFS(level, sum) {
+        if (level === numbers.length) {
+            if (sum === target) answer++
+            return
+        } 
+        else {
+            DFS(level + 1, sum + numbers[level]);
+            DFS(level + 1, sum - numbers[level])
+        }
+    }
+    
+    DFS(0, 0)
     return answer;
 }
