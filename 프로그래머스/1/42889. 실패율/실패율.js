7ffd8ed9a91718510c0adece36d1398e8b4b97f3
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-22 13:20:11 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,37 +1,17 @@
 function solution(N, stages) {
-    var answer = [];
-    const stageMap = new Map();
-    for (let stage of stages) {
-        if (stage > N) continue;
-        if (stageMap.has(stage)) stageMap.set(stage, stageMap.get(stage) + 1)
-        else stageMap.set(stage, 1);
-    };
+    let result = [];
+    let length = stages.length;
     
     for (let i = 1; i <= N; i++) {
-        if (!stageMap.has(i)) stageMap.set(i, 0)
+        const count = stages.filter(stage => stage === i).length;
+        const fail = length === 0 ? 0 : count / length;
+        result.push([i, fail]);
+        length -= count;
     }
     
-    const stageMapArr = [...stageMap];
+    // 실패율에 따라 내림차순으로 정렬
+    result.sort((a, b) => b[1] - a[1]);
     
-    stageMapArr.sort((a, b) => a[0] - b[0])
-    
-    const sortedStageMap = new Map(stageMapArr)
-    
-    let people = stages.length
-    const scoreMap = new Map();
-    
-    for (let [key, value] of sortedStageMap) {
-        scoreMap.set(key, value / people);
-        people -= value;
-    }
-    
-    const scoreMapArr = [...scoreMap];
-    scoreMapArr.sort((a, b) => {
-        if (a[1] === b[1]) return a[0] - b[0];
-        else return b[1] - a[1]
-    })
-    
-    answer = scoreMapArr.map(score => score[0])
-    
-    return answer
+    // 스테이지 번호만 추출하여 반환
+    return result.map(stage => stage[0]);
 }
