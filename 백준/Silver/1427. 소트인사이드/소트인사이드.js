4a44a06d3d0f000c81bd2c670a149142c4540b86
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-03-24 21:30:07 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -2,4 +2,18 @@ const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
 let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('').map(Number)
-console.log(input.sort((a, b) => b - a).join(''))
+
+let count = Array(10).fill(0)
+
+for (let x of input) {
+  count[x]++
+}
+
+let answer = ''
+for (let i = 9; i >= 0; i--) {
+  for (let j = 0; j < count[i]; j++) {
+    answer += i
+  }
+}
+
+console.log(answer)
