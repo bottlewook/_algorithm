59afecf8a31424d650008ab2283e13b3b4da014e
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-04-04 10:12:19 +0900

    [Silver IV] Title: 동전 0, Time: 252 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-const input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
-let [n, k] = input[0].split(' ').map(Number);
-const [_, ...rest] = input;
-const arr = rest.map(Number)
-
-let count = 0;
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+let target = Number(input[0].split(' ')[1])
+const arr = [];
+for (let i = 1; i < input.length; i++) {
+  arr.push(Number(input[i]))
+}
+let answer = 0;
 
 for (let i = arr.length - 1; i >= 0; i--) {
-  while (arr[i] <= k) {
-    k -= arr[i]
-    count++
+  while (arr[i] <= target) {
+    answer++
+    target -= arr[i]
   }
 }
-
-console.log(count)
+console.log(answer)
