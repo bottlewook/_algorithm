4843f2ccc3bd396faa0ba805dc576db205ef62d3
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-04-05 14:34:55 +0900

    [Silver III] Title: N과 M (1), Time: 2460 ms, Memory: 13368 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,29 +1,23 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-const input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ').map(item => Number(item))
-const [a, b] = input
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ').map(Number)
+const [N, M] = input
+const checkList = Array.from({ length: N }, () => 0)
+const answer = Array.from({ length: M }, () => 0)
 
-
-function solution(n, m) {
-  let ch = Array.from({ length: n }, () => 0);
-  let temp = Array.from({ length: m }, () => 0);;
-  function DFS(L) {
-    if (L === m) {
-      console.log(temp.join(' '))
-    }
-    else {
-      for (let i = 0; i < n; i++) {
-        if (ch[i] === 0) {
-          ch[i] = 1;
-          temp[L] = i + 1
-          DFS(L + 1);
-          ch[i] = 0;
-        }
+function DFS(level) {
+  if (level === M) console.log(answer.join(' '))
+  else {
+    for (let i = 0; i < N; i++) {
+      if (checkList[i] === 0) {
+        checkList[i] = 1;
+        answer[level] = i + 1
+        DFS(level + 1)
+        checkList[i] = 0;
       }
     }
   }
-  DFS(0);
 }
 
-solution(a, b)
+DFS(0)
