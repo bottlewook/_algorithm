3e9044725fb5bde58e80fc53f5cc5815aa8d2789
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-04-19 22:09:48 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-const input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
-let n = +input[0]
-const arr = input[1].split(' ').map(Number)
-arr.sort((a, b) => a - b)
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
+
+let sum = 0;
 let answer = 0;
-let temp = 0;
 for (let x of arr) {
-  temp += x
-  answer += temp
+  sum += x
+  answer += sum
 }
+
 console.log(answer)
