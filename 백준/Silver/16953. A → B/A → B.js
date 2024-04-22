a73c12612812162f047eeab042977ef51fd147eb
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-04-22 17:23:02 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,23 +1,25 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ').map(Number)
-let [A, B] = input
-let count = 1;
-let flag = false
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ').map(Number);
 
-while (A <= B) {
-  if (A === B) {
-    flag = true
-    break
-  }
-  if (B % 2 === 0) {
-    B /= 2
-  } else if (B % 10 === 1) {
-    B = parseInt(B / 10)
-  } else break
-  count++
+let [a, b] = input;
+let answer = 0;
+
+while (a < b) {
+    if (b % 10 === 1) {
+        // 1로 끝나는 경우, 1을 제거합니다.
+        b = Math.floor(b / 10);
+    } else if (b % 2 === 0) {
+        // 2로 나눌 수 있는 경우, 2로 나눕니다.
+        b /= 2;
+    } else {
+        // 위의 두 조건에 해당하지 않는 경우, A를 B로 만들 수 없습니다.
+        break;
+    }
+    answer++;
 }
 
-if (!flag) console.log(-1)
-else console.log(count)
+// 최종적으로 b가 a와 같다면, 연산 횟수에 1을 더한 값을 출력합니다.
+// 그렇지 않다면, -1을 출력합니다.
+console.log(a === b ? answer + 1 : -1);
