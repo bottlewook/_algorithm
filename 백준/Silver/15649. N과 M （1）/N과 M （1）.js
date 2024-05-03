84ffca8b76764c27211383cb230ea33c582afd6d
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-03 17:01:35 +0900

    [Silver III] Title: N과 M (1), Time: 2396 ms, Memory: 13688 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,23 +1,24 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ').map(Number)
-const [N, M] = input
-const checkList = Array.from({ length: N }, () => 0)
-const answer = Array.from({ length: M }, () => 0)
+const input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
 
-function DFS(level) {
+const [N, M] = input[0].split(' ').map(Number);
+const checkList = Array.from({ length: N }, () => 0);
+const answer = Array.from({ length: M }, () => 0);
+
+function dfs(level) {
   if (level === M) console.log(answer.join(' '))
   else {
     for (let i = 0; i < N; i++) {
       if (checkList[i] === 0) {
-        checkList[i] = 1;
+        checkList[i] = 1
         answer[level] = i + 1
-        DFS(level + 1)
-        checkList[i] = 0;
+        dfs(level + 1)
+        checkList[i] = 0
       }
     }
   }
 }
 
-DFS(0)
+dfs(0)
