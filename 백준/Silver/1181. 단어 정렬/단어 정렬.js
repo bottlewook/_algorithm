506f9ac9e6f3c64264f4d7694cf69ed48dcb1f8c
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-04 13:41:21 +0900

    [Silver V] Title: 단어 정렬, Time: 244 ms, Memory: 19736 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,25 +1,31 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
-const set = new Set()
-for (let i = 1; i < input.length; i++) {
-  set.add(input[i])
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+
+const N = Number(input[0]);
+
+let set = new Set()
+
+for (let i = 1; i <= N; i++) {
+  set.add(input[i].trim())
 }
 
 const arr = [...set]
+
 arr.sort((a, b) => {
-  if (a.length !== b.length) return a.length - b.length
-  else {
+  if (a.length === b.length) {
     if (a < b) return - 1
     else if (a > b) return 1
     else 0
   }
+  else return a.length - b.length
 })
 
 let answer = ''
-for (let x of arr) {
-  answer += x + '\n'
-}
 
-console.log(answer)
+arr.forEach((item) => {
+  answer += item + '\n'
+})
+
+console.log(answer.trim())
