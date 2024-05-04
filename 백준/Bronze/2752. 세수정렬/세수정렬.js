955bd0f965e06254662ea2a6104d56e654a9fe7e
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-04 11:24:22 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-const input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ');
-input.sort((a, b) => a - b)
-console.log(input.join(' '))
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+
+const arr = input[0].split(' ').map(Number)
+
+console.log(arr.sort((a, b) => a - b).join(' '))
