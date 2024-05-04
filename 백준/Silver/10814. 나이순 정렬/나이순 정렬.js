3eca0cbb50b4a605c563e530ae2a04f657521466
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-04 14:27:02 +0900

    [Silver V] Title: 나이순 정렬, Time: 412 ms, Memory: 47984 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,21 +1,25 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
-const arr = [];
-for (let i = 1; i < input.length; i++) {
-  arr.push(input[i].split(' '))
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+
+const N = Number(input[0]);
+
+let arr = [];
+
+for (let i = 1; i <= N; i++) {
+  let [age, name] = input[i].split(' ')
+  arr.push([Number(age), name])
 }
 
 arr.sort((a, b) => {
-  if (+a !== +b) {
-    return +a[0] - +b[0]
-  }
+  return a[0] - b[0]
 })
 
 let answer = ''
+
 for (let x of arr) {
   answer += x.join(' ') + '\n'
 }
 
-console.log(answer)
+console.log(answer.trim())
