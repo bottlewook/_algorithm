1f7e2289fc0f1d019c41d6257cea1e7df0b7f995
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-04 11:33:52 +0900

    [Silver V] Title: K번째 수, Time: 4592 ms, Memory: 564736 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
-let [N, K] = input[0].split(' ').map(Number)
-let arr = input[1].split(' ').map(Number).sort((a, b) => a - b)
-console.log(arr[K - 1]) 
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+
+const [N, K] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+arr.sort((a, b) => a - b)
+console.log(arr[K - 1])
