30a9490c20db3ca36cb6a3ef1162f3afada15748
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-04 13:20:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 496 ms, Memory: 59812 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -1,23 +1,25 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
-let [N, ...arr] = input
-const temp = [];
-for (let x of arr) {
-  temp.push(x.split(' ').map(Number))
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
+
+const N = Number(input[0]);
+
+let arr = [];
+
+for (let i = 1; i <= N; i++) {
+  arr.push(input[i].split(' ').map(Number))
 }
 
-temp.sort((a, b) => {
-  if (a[1] === b[1]) {
-    return a[0] - b[0]
-  } else {
-    return a[1] - b[1]
-  }
+arr.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0]
+  else return a[1] - b[1]
 })
 
 let answer = ''
-for (let x of temp) {
-  answer += x.join(' ') + '\n'
-}
-console.log(answer)
+
+arr.forEach((item) => {
+  answer += item.join(' ') + '\n'
+})
+
+console.log(answer.trim())
