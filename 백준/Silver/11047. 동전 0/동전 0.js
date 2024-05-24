fd0279617aac691f16922f0e9cb8623bc798941d
임병욱 <103362820+bottlewook@users.noreply.github.com>
2024-05-24 20:51:54 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,16 +1,23 @@
 const fs = require("fs");
 const readFileSyncAddress = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
-let target = Number(input[0].split(' ')[1])
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
+
+let [N, K] = input[0].split(' ').map(Number);
+
 const arr = [];
-for (let i = 1; i < input.length; i++) {
-  arr.push(Number(input[i]))
-}
+
 let answer = 0;
 
-for (let i = arr.length - 1; i >= 0; i--) {
-  answer += parseInt(target / arr[i])
-  target %= arr[i]
+for (let i = 1; i <= N; i++) {
+  arr.push(Number(input[i]))
 }
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  if (arr[i] <= K) {
+    answer += parseInt(K / arr[i])
+    K = K % arr[i]
+  }
+}
+
 console.log(answer)
